7389147d0a9ca17f6f7f65042714592931204a4f
Murilo Ijanc <mbsd@m0x.ru>
2023-06-06 23:09:33 -0300

    chore: Refactoring architecture

--- platform_architecture.py
+++ platform_architecture.py
@@ -1,32 +1,45 @@
-from diagrams import Cluster, Diagram
-from diagrams.aws.compute import ECS, Fargate, ECR
-from diagrams.aws.database import RDS
-from diagrams.aws.network import Route53, PrivateSubnet, PublicSubnet, NATGateway, InternetGateway, ALB
-from diagrams.aws.network import VPCElasticNetworkInterface
+#!/usr/bin/env python3
+#
+# Copyright (c) 2023 Murilo Ijanc' <mbsd@m0x.ru>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#
 
-with Diagram("Microbio Platform Architecture", show=True):
-    with Cluster("Region"):
-        with Cluster("VPC", direction="LR"):
+from diagrams import Cluster, Diagram, Edge
+# aws compute
+from diagrams.aws.compute import (ECR, ECS, ElasticContainerServiceService,
+                                  Fargate)
+# aws database
+from diagrams.aws.database import DDB, RDS, ElastiCache
+# aws devtools
+from diagrams.aws.devtools import XRay
+# aws engagement
+from diagrams.aws.engagement import SES
+# general
+from diagrams.aws.general import User, Client
+# aws integration
+from diagrams.aws.integration import SNS, SQS
+# aws management
+from diagrams.aws.management import Cloudwatch
+# aws network
+from diagrams.aws.network import (ALB, CF, AppMesh, InternetGateway,
+                                  NATGateway, PrivateSubnet, PublicSubnet,
+                                  Route53, VPCElasticNetworkInterface)
+# aws security
+from diagrams.aws.security import ACM, IAM, KMS, WAF, SecretsManager
+# aws storage
+from diagrams.aws.storage import S3
 
-
-            with Cluster("Availability Zone B"):
-                with Cluster("Public subnet B"):
-                    nta = NATGateway()
-                with Cluster("Private subnet B"):
-                    ecsa = ECS() >> Fargate() >> VPCElasticNetworkInterface() >> nta
-
-
-            with Cluster("Availability Zone A"):
-                with Cluster("Public subnet A"):
-                    ntb = NATGateway()
-                with Cluster("Private subnet A"):
-                    ecsb = ECS() >> Fargate() >> VPCElasticNetworkInterface() >> ntb
-
-            ig = InternetGateway()
-            alb = ALB()
-            ig >> alb
-            # ecsb = ECS() >> Fargate() >> VPCElasticNetworkInterface() >> ntb
-            nta >> ig \
-                    << ntb
-            ecsa << alb >> ecsb
-        # ecr = ECR()
+with Diagram("Microbio Platform Architecture", show=False):
+    user = User("User1")
+    client = Client("Client1")
